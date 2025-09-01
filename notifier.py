51b4e5748fb1777ae8ab9e4acb6088ad90fa85f2
Martin Molin <martin@brandmetrics.com>
2025-09-01 08:37:57 +0200

    add smtplib, success sending email

--- notifier.py
+++ notifier.py
@@ -1,11 +1,23 @@
 import smtplib
 import config
+import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
 def send_mail(subject, message):
     script_config = config.read_config()
-    print(f'To: {script_config['email']}')
-    print(f'Sending mail: {subject}')
-    print(f'Message: {message}')
+    password = os.environ.get('EMAIL_PASSWORD')
+    if password is None:
+        raise ValueError('Missing EMAIL_PASSWORD env variable')
+    try:
+        with smtplib.SMTP(script_config['smtp_server'], script_config['smtp_port']) as server:
+            server.starttls()
+            server.login(script_config['from_email'], password)
+            msg = MIMEText(message)
+            msg['Subject'] = subject
+            msg['From'] = script_config['from_email']
+            msg['To'] = script_config['to_email']
+            server.sendmail(script_config['from_email'], script_config['to_email'], msg.as_string())
+    except Exception as e:
+        print(f'Failed to send email: {e}')
